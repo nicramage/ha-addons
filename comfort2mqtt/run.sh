0eb6c70097f30b940cc7c0de9539519c4c5eeb02
Nic Ramage <nic@ramage.co.za>
2023-09-27 11:52:31 +0200

    Retrieve overridden mqtt settings from config

--- comfort2mqtt/run.sh
+++ comfort2mqtt/run.sh
@@ -17,10 +17,23 @@ if bashio::config.has_value "raw_msgs"; then
   raw_msgs=$(bashio::config 'raw_msgs')
 fi
 
+# Get MQTT settings from the MQTT service, if available
 if bashio::services.available "mqtt"; then
   host=$(bashio::services "mqtt" "host")
   user=$(bashio::services "mqtt" "username")
   password=$(bashio::services "mqtt" "password")
+fi
+
+# Override MQTT settings with anything explicitly specified
+# in the config.
+if bashio::config.has_value "mqtt_host"; then
+  host=$(bashio::config 'mqtt_host')
+fi
+if bashio::config.has_value "mqtt_user"; then
+  user=$(bashio::config 'mqtt_user')
+fi
+if bashio::config.has_value "mqtt_password"; then
+  user=$(bashio::config 'mqtt_user')
 fi
 
 export COMFORT2MQTT_MQTT_HOST=${host}
